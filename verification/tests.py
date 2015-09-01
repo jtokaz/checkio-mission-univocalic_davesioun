6591227814b199f85340d7842cf076d05fba12a0
jtokaz <jtokaz@gmail.com>
2015-09-01 18:33:58 -0500

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -1,16 +1,14 @@
 import random
 
-def factorial(num):
-    # This is very basic solution.
-    return num * factorial(num - 1) if num else 1
-
-
 TESTS = {"Basic": [
-    {"input": 0, "answer": 1},
-    {"input": 1, "answer": 1},
-    {"input": 2, "answer": 2},
+    {"input": [0,11], "answer": 11},
+    {"input": [14,0], "answer": 14},
+    {"input": [22,4], "answer": 22},
 ]}
 
-
-for n in random.sample(range(3, 100), 30):
-    TESTS["Basic"].append({"input": n, "answer": factorial(n)})
+R = range(0,100)
+for x,y in zip(random.sample(R,10),random.sample(R,10)):
+    TESTS["Basic"].append({"input": [x,y], "answer": max(x,y)})
+R = range(0,2000000001)
+for x,y in zip(random.sample(R,20),random.sample(R,20)):
+    TESTS["Basic"].append({"input": [x,y], "answer": max(x,y)})
