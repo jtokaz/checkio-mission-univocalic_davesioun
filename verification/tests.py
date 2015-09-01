016b3d15e8a7ac89e444e99687b7c08151e58f7f
jtokaz <jtokaz@gmail.com>
2015-09-01 18:55:36 -0500

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -6,9 +6,9 @@ TESTS = {"Basic": [
     {"input": [22,10], "answer": 22},
 ]}
 
-#R = range(0,100)
-#for x,y in zip(random.sample(R,10),random.sample(R,10)):
-#    TESTS["Basic"].append({"input": [x,y], "answer": max(x,y)})
-#R = range(0,2000000001)
-#for x,y in zip(random.sample(R,20),random.sample(R,20)):
-#    TESTS["Basic"].append({"input": [x,y], "answer": max(x,y)})
+R = range(0,100)
+for x,y in zip(random.sample(R,10),random.sample(R,10)):
+    TESTS["Basic"].append({"input": [x,y], "answer": max(x,y)})
+R = range(0,2000000001)
+for x,y in zip(random.sample(R,20),random.sample(R,20)):
+    TESTS["Basic"].append({"input": [x,y], "answer": max(x,y)})
