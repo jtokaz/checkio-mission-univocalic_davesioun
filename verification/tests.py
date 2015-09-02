8d7f126a9cad9e1279553e3fecbcd0177852ae09
jtokaz <jtokaz@gmail.com>
2015-09-01 23:10:49 -0500

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -1,14 +1,16 @@
 import random
 
 TESTS = {"Basic": [
-    {"input": [0,11], "answer": 11},
-    {"input": [14,0], "answer": 14},
-    {"input": [22,10], "answer": 22},
+    {"input": 0, "answer": 0},
+    {"input": 9, "answer": 0},
+    {"input": 41, "answer": 4},
+    {"input": 65, "answer": 6},
+    {"input": 79, "answer": 7},
 ]}
 
-R = range(0,100)
-for x,y in zip(random.sample(R,10),random.sample(R,10)):
-    TESTS["Basic"].append({"input": [x,y], "answer": max(x,y)})
+
+for x in random.sample(range(0,1000),10):
+    TESTS["Basic"].append({"input": x, "answer": x//10})
 R = range(0,2000000001)
-for x,y in zip(random.sample(R,20),random.sample(R,20)):
-    TESTS["Basic"].append({"input": [x,y], "answer": max(x,y)})
+for x in random.sample(range(0,2000000000),30):
+    TESTS["Basic"].append({"input": x, "answer": x//10})
