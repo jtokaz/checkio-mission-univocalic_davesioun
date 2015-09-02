d3c7af1950a36622b6a20b2ea7ce715b13a583f3
jtokaz <jtokaz@gmail.com>
2015-09-01 23:29:05 -0500

    Update init.js

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -79,7 +79,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             }
 
             //YOUR FUNCTION NAME
-            var fname = 'univocalic_davasaan';
+            var fname = 'davasaan';
 
             var checkioInput = data.in || 1;
             var checkioInputStr = fname + '(' + JSON.stringify(checkioInput) + ')';
